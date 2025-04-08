f5cf9a89acb5f1b7a9085bfe50ae13603c593643
Mackroph <205552813+CristianMz21@users.noreply.github.com>
2025-04-08 16:47:35 -0500

    creadas vistas y templates

--- Tareas/views.py
+++ Tareas/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from .models import Tarea
+from .forms import TareaForm
 
-# Create your views here.
+class TareaListView(ListView):
+    model = Tarea
+    template_name = 'tarea_list.html'  
+    context_object_name = 'tareas'
+
+    def get_queryset(self):
+        return Tarea.objects.all().order_by('-fecha_creacion')
+
+class TareaCreateView(CreateView):
+    model = Tarea
+    form_class = TareaForm
+    template_name = 'tarea_form.html'
+    success_url = reverse_lazy('tarea_list')
